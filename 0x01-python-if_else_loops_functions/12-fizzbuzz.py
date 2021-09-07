c3936cf170e3fd5d965df492ffce7e005e159ee0
jeremy302 <olajeremy123@gmail.com>
2021-09-07 15:16:49 +0100

    patch

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,4 +3,4 @@ def fizzbuzz():
     print(''.join('%s ' %
                   ('FizzBuzz' if not i % 15 else 'Buzz' if not i % 5
                    else 'Fizz' if not i % 3 else str(i))
-                  for i in range(1, 101)))
+                  for i in range(1, 101)), end = '')
--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def islower(c):
-    return bool(bool(c) and type(c) == str and len(c) == 1 and
+    return (bool(c) and type(c) == str and len(c) == 1 and
                 ord(c) >= ord('a') and ord(c) <= ord('z'))
