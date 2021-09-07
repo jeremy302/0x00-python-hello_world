7b2791c0c467d492c411d2bd202a37e7e191004a
jeremy302 <olajeremy123@gmail.com>
2021-09-07 12:17:29 +0100

    patch

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import random
-number = -626#random.randint(-10000, 10000)
+number = random.randint(-10000, 10000)
 last_digit = number % 10 if number > 0 else -(-number % 10)
 range_str = 'greater than 5' if last_digit > 5\
     else '0' if last_digit == 0 else 'less than 6 and not 0'
