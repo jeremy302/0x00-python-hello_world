c2d3a04e756d3151eb54780e32236cab62a7769e
jeremy302 <olajeremy123@gmail.com>
2021-09-07 14:45:55 +0100

    patch

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    print(' '.join('%s' %
+    print(''.join('%s ' %
                    ('FizzBuzz' if not i % 15 else 'Buzz' if not i % 5
                     else 'Fizz' if not i % 3 else str(i))
                    for i in range(1, 101)))
+fizzbuzz()
