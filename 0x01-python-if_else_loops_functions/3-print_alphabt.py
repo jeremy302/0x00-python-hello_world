6c56a8c9e89ba0dc39c3700345f7dc0eda5cd548
jeremy302 <olajeremy123@gmail.com>
2021-09-07 12:03:43 +0100

    patch

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for c in range(ord('a'), ord('z') + 1):
-    if chr(c) not in ['q', 'e']:
-        print(chr(c), end='')
+# for c in range(ord('a'), ord('z') + 1):
+#     if chr(c) not in ['q', 'e']:
+#         print(chr(c), end='')
+print('abcdefghijklmnopqrstuvwxyz', end='')
