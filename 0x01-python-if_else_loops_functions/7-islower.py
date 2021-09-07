e9bba6789d1b82190ed27066ad6fb846b20d42c3
jeremy302 <olajeremy123@gmail.com>
2021-09-07 15:45:24 +0100

    patch

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def islower(c):
-    return bool(c == '' or any(_c >= 'a' and _c <= 'z' for _c in c))
+    return bool(c in [' ', ''] or any(_c >= 'a' and _c <= 'z' for _c in c))
