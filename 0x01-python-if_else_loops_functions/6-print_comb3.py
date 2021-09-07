e40d836a3d775b92a7d72f9f1cb44aca40f84cb1
jeremy302 <olajeremy123@gmail.com>
2021-09-07 12:43:58 +0100

    patch

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-print(', '.join([', '.join('%d%d' % (i, j) for j in range(10)
+print(', '.join([', '.join('{:d}{:d}'.format(i, j) for j in range(10)
                            if j > i) for i in range(9)]))
