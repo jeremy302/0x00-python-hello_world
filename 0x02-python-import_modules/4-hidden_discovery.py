aebb2f0a14c2fcee2a830e9833517e568ffde839
jeremy302 <olajeremy123@gmail.com>
2021-09-09 15:58:16 +0100

    patch

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 import sys
+import re
 import hidden_4
 
 if __name__ == '__main__':
-    for name in (dir(hidden_4)):
-        if not name.startswith('__'):
-            print('{:s}'.format(name))
+    with open('hidden_4.pyc', 'rb') as f:
+        h = f.read()
+        for n in sorted(re.findall(rb'\w{3,}', h)):
+            if n not in [b'hidden_4', b'module'] and\
+               not n.startswith(b'__'):
+                print(n.decode())
