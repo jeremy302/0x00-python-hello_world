065c5628bbc1ab762cf6a4842140ed2367eb9771
jeremy302 <olajeremy123@gmail.com>
2021-09-09 11:20:32 +0100

    patch

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 import sys
 import hidden_4
-if not (sys.version_info.major == 3 and sys.version_info.minor == 4):
-    exit(1)
 
 if __name__ == '__main__':
     for name in sorted(dir(hidden_4)):
