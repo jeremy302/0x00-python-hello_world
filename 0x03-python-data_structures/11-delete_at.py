491e81a42807f9965c296e605009237fab50af2d
jeremy302 <olajeremy123@gmail.com>
2021-09-12 06:02:19 +0100

    patch

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     ls = my_list.copy()
-    my_list.clear
+    my_list.clear()
     for i in range(len(ls)):
         if i != idx:
             my_list.append(ls[i])
