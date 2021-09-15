25f071185010b13d73fa5175705a4926b60ed387
jeremy302 <olajeremy123@gmail.com>
2021-09-15 23:10:44 +0100

    patch

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -3,4 +3,7 @@ def complex_delete(a_dictionary, value):
     r = {k: v for k, v in a_dictionary.items() if v != value}
     if list(r.keys()) == list(a_dictionary.keys()):
         return a_dictionary
+    for k in list(a_dictionary.keys()):
+        if k not in r:
+            del a_dictionary[k]
     return r
