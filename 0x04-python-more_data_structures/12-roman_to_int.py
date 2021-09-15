d5ece42b34784d9bdcb2f55be3d76cbd6c1a0bc1
jeremy302 <olajeremy123@gmail.com>
2021-09-15 23:15:36 +0100

    patch

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     if type(roman_string) is not str or not roman_string:
         return None
-    roman_sting = roman_string.lower()
+    roman_string = roman_string.lower()
     tbl = {'': 0, 'i': 1, 'v': 5, 'x': 10,
            'l': 50, 'c': 100, 'd': 500, 'm': 1000}
     i = 0
