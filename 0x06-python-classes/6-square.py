d6253b62cc9b7ba2783f2d011802d3d4ad076ea0
jeremy302 <olajeremy123@gmail.com>
2021-09-28 14:01:25 +0100

    added check for a negative position

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -46,6 +46,6 @@ class Square:
     def position(self, value):
         ''' sets the position of the square '''
         if type(value) != tuple or len(value) != 2 or \
-           value[0] < 0 or value[0] < 0:
+           value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
