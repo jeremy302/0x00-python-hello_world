63872a8b32c6731b823c8432929601464d3fe0d5
jeremy302 <olajeremy123@gmail.com>
2021-10-04 16:48:44 +0100

    added missing `raise` keyword for `bigger_or_equal()` method

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -73,7 +73,7 @@ class Rectangle:
     def bigger_or_equal(rect_1, rect_2):
         ''' compares the area of 2 rectangles '''
         if type(rect_1) is not Rectangle:
-            TypeError("rect_1 must be an instance of Rectangle")
+            raise TypeError("rect_1 must be an instance of Rectangle")
         if type(rect_2) is not Rectangle:
-            TypeError("rect_2 must be an instance of Rectangle")
+            raise TypeError("rect_2 must be an instance of Rectangle")
         return rect_1 if rect_1.area() >= rect_2.area() else rect_2
