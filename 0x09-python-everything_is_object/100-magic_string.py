08fd00ebd16510971e522dfb5b571e6eb67956fe
jeremy302 <olajeremy123@gmail.com>
2021-10-05 20:36:44 +0100

    added to __slots__ to locked class

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
     globals().update({'mag_i': globals().get('mag_i', 0) + 1})
-    return ("BestSchool, " * globals().get('mag_i'))[:-2]
+    return ("Holberton, " * (globals().get('mag_i')))[:-2]
