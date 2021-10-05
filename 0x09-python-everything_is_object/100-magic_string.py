fd579fcb6a2f10cc651ed294f716a279e24c7fb4
jeremy302 <olajeremy123@gmail.com>
2021-10-05 18:05:52 +0100

    trying different method

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    magic_string.__dict__['i'] = magic_string.__dict__.get('i', 0) + 1
-    return ("BestSchool, " * magic_string.__dict__['i'])[:-2]
+    globals().update({'mag_i': globals().get('mag_i', 0) + 1})
+    return ("BestSchool, " * globals().get('mag_i'))[:-2]
--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -2,11 +2,29 @@
 ''' module for a locked class'''
 
 
+class LockedDict(dict):
+    ''' A restrictive dict '''
+    def __setitem__(self, name, value):
+        ''' sets an key-value pair '''
+        if name != 'first_name':
+            raise AttributeError("object has no attribute {}".format(
+                repr(name)))
+        super().__setitem__(name, value)
+
+
 class LockedClass:
     ''' A class that allows certain attributes to be set '''
+
     def __setattr__(self, name, value):
         ''' controls which attributes can be set '''
         if name != 'first_name':
             raise AttributeError("object has no attribute {}".format(
                 repr(name)))
         super().__setattr__(name, value)
+
+    def __setitem__(self, name, value):
+        ''' controls which attributes can be set with index notation '''
+        if name != 'first_name':
+            raise AttributeError("object has no attribute {}".format(
+                repr(name)))
+        self.first_name = value
