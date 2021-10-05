25bcfb2aba16d79f3c6aa3fe21f216df54192069
jeremy302 <olajeremy123@gmail.com>
2021-10-05 13:29:08 +0100

    added documentation

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+''' module for a locked class'''
+
+
 class LockedClass:
+    ''' A class that allows certain attributes to be set '''
     def __setattr__(self, name, value):
+        ''' controls which attributes can be set '''
         if name != 'first_name':
             raise AttributeError("object has no attribute 'last_name'")
         super().__setattr__(name, value)
