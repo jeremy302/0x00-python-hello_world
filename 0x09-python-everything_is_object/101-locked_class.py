645a4aceb0db23eba9ca2b9d0f7adaa603f72927
jeremy302 <olajeremy123@gmail.com>
2021-10-05 12:41:09 +0100

    added 0x09

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+class LockedClass:
+    def __setattr__(self, name, value):
+        if name != 'first_name':
+            raise AttributeError("object has no attribute 'last_name'")
+        super().__setattr__(name, value)
---
+++ 0x09-python-everything_is_object/README.md
@@ -0,0 +1,3 @@
+# Project 0x09. Python - Everything is object
+
+Answers to the questions in each task
