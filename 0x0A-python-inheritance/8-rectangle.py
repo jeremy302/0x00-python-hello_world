8447262cd37d3b3ab1ce3810b05d909d70b57c02
jeremy302 <olajeremy123@gmail.com>
2021-10-11 15:17:35 +0100

    using type() to validate

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -11,7 +11,7 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         ''' validates an integer '''
-        if not issubclass(type(value), int):
+        if type(value) is not int:
             raise TypeError('{:s} must be an integer'.format(name))
         if value <= 0:
             raise ValueError('{:s} must be greater than 0'.format(name))
