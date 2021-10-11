1b14745d35bd3cb91ff9c81ff219bd6aa5ef83ca
jeremy302 <olajeremy123@gmail.com>
2021-10-11 17:17:38 +0100

    added default params

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -19,15 +19,10 @@ class BaseGeometry:
 
 class Rectangle(BaseGeometry):
     ''' a rectangle class '''
-    def __init__(self, width, height):
+    def __init__(self, width=1, height=1):
         ''' constructs a rectangle '''
-        if type(width) is not int:
-            raise TypeError('width must be an integer')
-        if type(height) is not int:
-            raise TypeError('height must be an integer')
-        if width <= 0:
-            raise ValueError('width must be greater than 0')
-        if height <= 0:
-            raise ValueError('height must be greater than 0')
         self.__width = width
         self.__height = height
+
+        self.integer_validator('width', width)
+        self.integer_validator('height', height)
