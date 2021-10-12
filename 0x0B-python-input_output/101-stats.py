2339b43b88bd5b7aa7178b25750b2b48d0dd8cf0
jeremy302 <olajeremy123@gmail.com>
2021-10-12 23:01:02 +0100

    new main method

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -2,62 +2,43 @@
 ''' script for processing requests logs '''
 import signal
 import re
-import sys
+from sys import stdin
 
 count = 0
 status_codes = {'200': 0, '301': 0, '400': 0, '401': 0,
                 '403': 0, '404': 0, '405': 0, '500': 0}
-total_file_size = 0
+total_size = 0
 
 
 def print_logs():
     ''' prints the logs '''
-    global status_codes, total_file_size
-
-    print('File size: {}'.format(total_file_size))
-    for status_code in sorted(status_codes.keys()):
-        if status_codes[status_code]:
-            print('{}: {}'.format(status_code, status_codes[status_code]))
-
-
-def sigint(sig, sframe):
-    ''' SIGINT handler '''
-    if count % 10:
-        print_logs()
-    signal.default_int_handler()
-    exit(1)
+    global status_codes, total_size
+    print('File size: {}'.format(total_size))
+    for key, val in sorted(status_codes.items(), key=lambda v: v[0]):
+        if val:
+            print('{}: {}'.format(key, val))
 
 
 def main():
     ''' logs network requests '''
-    global count, status_codes, total_file_size
-
-    # signal.signal(signal.SIGINT, sigint)
-    r = re.compile(r'(?P<ip_address>.*) - \[(?P<date>.+)\] ' +
-                   r'"GET /projects/260 HTTP/1.1" ' +
-                   r'(?P<status_code>\d+) (?P<file_size>\d+)\w*$')
-    while True:
-        count += 1
-        try:
-            inp = input()
-            match = r.match(inp).groupdict()
-            spl = inp.split(' ')
-            if len(spl) < 2:
+    global count, status_codes, total_size
+    try:
+        for line in stdin:
+            parts = line.split(' ')
+            if len(parts) < 2:
                 continue
-        except EOFError:
-            print_logs()
-            exit()
-        except KeyboardInterrupt as er:
-            print_logs()
-        status_code = spl[-2]  # int(match['status_code'])
-        # if status_code in status_codes:
-        if status_code in status_codes:
+            status_code = parts[-2]
+            file_size = parts[-1]
+            if status_code not in status_codes:
+                continue
             status_codes[status_code] += 1
-        else:
-            status_codes[status_code] = 1
-        total_file_size += int(spl[-1])  # int(match['file_size'])
-        if not count % 10:
-            print_logs()
+            total_size += int(file_size)
+            count += 1
+            if count % 10 == 0:
+                print_logs()
+        print_logs()
+    except KeyboardInterrupt:
+        print_logs()
 
 if __name__ == '__main__':
     main()
