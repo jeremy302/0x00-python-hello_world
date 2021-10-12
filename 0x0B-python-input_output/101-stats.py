6ae3ee4064ad8183a55175843fc7b878e2134c36
jeremy302 <olajeremy123@gmail.com>
2021-10-12 22:23:54 +0100

    removed format specifiers

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -5,7 +5,7 @@ import re
 import sys
 
 count = 0
-status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+status_codes = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
 total_file_size = 0
 
 
@@ -13,10 +13,10 @@ def print_logs():
     ''' prints the logs '''
     global status_codes, total_file_size
 
-    print('File size: {:d}'.format(total_file_size))
+    print('File size: {}'.format(total_file_size))
     for status_code in sorted(status_codes.keys()):
         if status_codes[status_code]:
-            print('{:d}: {:d}'.format(status_code, status_codes[status_code]))
+            print('{}: {}'.format(status_code, status_codes[status_code]))
 
 
 def sigint(sig, sframe):
@@ -48,10 +48,12 @@ def main():
             exit()
         except KeyboardInterrupt as er:
             print_logs()
-            raise er
-        status_code = int(spl[-2])  # int(match['status_code'])
+        status_code = spl[-2]  # int(match['status_code'])
         # if status_code in status_codes:
-        status_codes[status_code] += 1
+        if status_code in status_codes:
+            status_codes[status_code] += 1
+        else:
+            status_codes[status_code] = 1
         total_file_size += int(spl[-1])  # int(match['file_size'])
         if not count % 10:
             print_logs()
