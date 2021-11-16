809b96f907aabc684c6dfd9e172967ca0d9c23e5
jeremy302 <olajeremy123@gmail.com>
2021-11-16 20:16:41 +0100

    where patch

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -2,4 +2,4 @@
 -- inserts a row
 INSERT INTO first_table (`id`, `name`) VALUES (89, 'Holberton School');
 -- print rows
-SELECT `id`, `name` FROM first_table;
+SELECT `id`, `name` FROM first_table WHERE id = 89;
