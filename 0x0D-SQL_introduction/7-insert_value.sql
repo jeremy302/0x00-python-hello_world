e5402e48f54ddb5e0c0a4e34cf92f6ae83d59f7a
jeremy302 <olajeremy123@gmail.com>
2021-11-16 20:22:53 +0100

    removed col names

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,5 +1,3 @@
 -- inserts a new row into `first_table`
 -- inserts a row
-INSERT INTO first_table (`id`, `name`) VALUES (89, 'Holberton School');
--- print rows
-SELECT `id`, `name` FROM first_table WHERE id = 89;
+INSERT INTO first_table VALUES (89, 'Holberton School');
