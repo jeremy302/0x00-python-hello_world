14fbc9357fd957f252ae146bf0253e1dd83b2f47
jeremy302 <olajeremy123@gmail.com>
2021-11-16 21:33:15 +0100

    made keywords uppercase

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,3 +1,3 @@
 -- queries for max temperatures by state, ordered by state
 -- max temperatures by states
-SELECT state, MAX(value) as max_temp from temperatures GROUP BY state ORDER BY state ASC;
+SELECT state, MAX(value) AS max_temp FROM temperatures GROUP BY state ORDER BY state ASC;
