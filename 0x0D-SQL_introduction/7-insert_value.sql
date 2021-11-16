72b963d629043d3c8a4b7d3965c3847766e989fb
jeremy302 <olajeremy123@gmail.com>
2021-11-16 20:09:56 +0100

    lowercase

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,5 +1,3 @@
 -- inserts a new row into `first_table`
 -- inserts a row
-INSERT INTO first_table (`id`, `name`) VALUES (89, 'Holberton');
--- print rows
-SELECT `id`, `name` FROM first_table;
+INSERT INTO first_table (`id`, `name`) VALUES (89, 'holberton');
