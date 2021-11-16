85a53f0d5e8703a70e5c72050158d2a01d411826
jeremy302 <olajeremy123@gmail.com>
2021-11-16 20:08:46 +0100

    db is printed after insertion

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,5 @@
 -- inserts a new row into `first_table`
 -- inserts a row
-INSERT INTO first_table (`id`, `name`) VALUES (89, 'holberton');
+INSERT INTO first_table (`id`, `name`) VALUES (89, 'Holberton');
+-- print rows
+SELECT `id`, `name` FROM first_table;
