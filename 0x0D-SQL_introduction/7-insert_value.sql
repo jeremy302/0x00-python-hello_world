f8891153bf720b28ea4e2dfdd5c37b5fa3f74cc2
jeremy302 <olajeremy123@gmail.com>
2021-11-16 20:14:35 +0100

    patch

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,5 @@
 -- inserts a new row into `first_table`
 -- inserts a row
 INSERT INTO first_table (`id`, `name`) VALUES (89, 'Holberton School');
+-- print rows
+SELECT `id`, `name` FROM first_table;
