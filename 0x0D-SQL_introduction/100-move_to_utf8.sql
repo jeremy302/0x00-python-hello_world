c1269211114dc8e51be34c05045e4bd3d7137c26
jeremy302 <olajeremy123@gmail.com>
2021-11-16 21:43:49 +0100

    patch

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,6 +1,8 @@
 -- converts database to utf 8
 -- convert db
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- switch db
+USE hbtn_0c_0;
 -- converts table
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
