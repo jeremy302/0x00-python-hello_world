6ef26e31cd8b5b189c0216c0ff517ecba34979cd
jeremy302 <olajeremy123@gmail.com>
2021-11-18 01:18:06 +0100

    task 16 fix

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- query db
 SELECT tv_shows.title AS tile, tv_genres.name AS name FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = show_id
-LEFT JOIN tv_genres ON tv_genres.id = genre_id
+LEFT JOIN tv_genres ON tv_shows.id = show_id AND tv_genres.id = genre_id
 ORDER BY tv_shows.title ASC, tv_genres.name ASC;
