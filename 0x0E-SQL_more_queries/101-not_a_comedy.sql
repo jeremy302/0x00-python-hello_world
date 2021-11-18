47dcea86e0f2eb1c5a51b6ff7ac011d08b8f25a7
jeremy302 <olajeremy123@gmail.com>
2021-11-18 01:05:09 +0100

    made syntax keywords uppercase

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,6 +1,6 @@
 -- queries db
 SELECT title FROM tv_shows WHERE id NOT IN
-(SELECT DISTINCT(tv_shows.id) as id FROM tv_shows, tv_show_genres, tv_genres
+(SELECT DISTINCT(tv_shows.id) AS id FROM tv_shows, tv_show_genres, tv_genres
 WHERE tv_shows.id = tv_show_genres.show_id
 AND tv_show_genres.genre_id = tv_genres.id AND tv_genres.name = 'Comedy')
 ORDER BY title ASC;
