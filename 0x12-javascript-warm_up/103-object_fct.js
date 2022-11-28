81bc050e2e5bd81ac3bf6d5b2690a4d84985979f
jeremy302 <olajeremy123@gmail.com>
2022-11-28 12:54:45 +0100

    patch

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,2 +1,15 @@
 #!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+#!/usr/bin/node
 myObject.incr = () => myObject.value++;
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+
