922aae82d958e47f85a9d548935b43a3f6a57ed3
jeremy302 <olajeremy123@gmail.com>
2022-11-29 14:15:23 +0100

    fixed newlines

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -5,6 +5,6 @@ const fs = require('fs');
 const [f1, f2, f3] = process.argv.slice(2);
 fs.readFile(f1, (_, d1) => {
   fs.readFile(f2, (_, d2) => {
-    fs.writeFile(f3, d1 + '\n' + d2 + '\n', () => {});
+    fs.writeFile(f3, d1 + d2, () => {});
   });
 });
