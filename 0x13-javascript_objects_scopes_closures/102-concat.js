4dc9aa30dd354a2e33f52c9a6918602c346e536b
jeremy302 <olajeremy123@gmail.com>
2022-11-29 13:06:03 +0100

    added const

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -2,9 +2,9 @@
 
 const fs = require('fs');
 
-[f1, f2, f3] = process.argv.slice(2);
-fs.readFile(f1, (_, d1)=>{
-	fs.readFile(f2, (_, d2)=>{
-		fs.writeFile(f3, d1 +'\n'+ d2 + '\n', ()=>{});
-	});
+const [f1, f2, f3] = process.argv.slice(2);
+fs.readFile(f1, (_, d1) => {
+  fs.readFile(f2, (_, d2) => {
+    fs.writeFile(f3, d1 + '\n' + d2 + '\n', () => {});
+  });
 });
