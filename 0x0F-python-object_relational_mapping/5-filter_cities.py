8aed9103c733e4a6682d32d86ea937c0387b0498
jeremy302 <olajeremy123@gmail.com>
2022-12-02 10:31:23 +0100

    patch

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -7,14 +7,19 @@ if __name__ == '__main__':
     host = 'localhost'
     user, passwd, dbname, state = sys.argv[1:5]
 
-    db = connect(host=host, user=user, passwd=passwd, db=dbname)
-    cur = db.cursor()
-    cur.execute('''select cities.name from cities where cities.state_id = ''' +
-                '''(select id from states where states.name='{0}') '''
-                .format(state) +
-                'order by cities.id')
-    rows = list(cur.fetchall())
-    if rows:
-        print(', '.join(row[0] for row in rows))
-    cur.close()
-    db.close()
+    try:
+        db = connect(host=host, user=user, passwd=passwd, db=dbname)
+        cur = db.cursor()
+        cur.execute(
+            '''select cities.name from cities where cities.state_id = ''' +
+            '''(select id from states where states.name='{0}') '''
+            .format(state) +
+            'order by cities.id')
+        rows = list(cur.fetchall())
+        if rows:
+            print(', '.join(row[0] for row in rows), end='')
+        cur.close()
+        db.close()
+    except Exception:
+        pass
+    print()
