abae325783ecbb0f81bcbb503844bcd8186696cb
jeremy302 <olajeremy123@gmail.com>
2022-12-15 18:16:29 +0100

    patch

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # answer file
 curl -s -X PUT -L -H 'Origin: School' -d "user_id=98" "0.0.0.0:5000/catch_me"
+curl -s -H 'Origin: School' -L -d "user_id=98" "0.0.0.0:5000/catch_me" -X PUT -o /dev/null -w 'You got me!'
