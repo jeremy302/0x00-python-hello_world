aca351185a36ab6d44ca23469e38a304c93f1a8b
jeremy302 <olajeremy123@gmail.com>
2022-12-15 20:14:27 +0100

    patch

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # answer file
-curl -s -X PUT -L --max-redirs -1 -H 'Origin: HolbertonSchool' -d "user_id=98" "0.0.0.0:5000/catch_me"
+curl -s -X PUT -L -H 'Origin: HolbertonSchool' -d "user_id=98" "0.0.0.0:5000/catch_me"
