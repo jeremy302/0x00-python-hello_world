6da2918bd7b85e39ade9db235edf67fbef1c304b
jeremy302 <olajeremy123@gmail.com>
2022-12-15 16:35:12 +0100

    patch

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 ''' contains find_peak function '''
 
+
 def find_peak(list_of_integers):
     ''' finds a peak in the list '''
-    return max(list_of_integers or [None])
+    return sorted(list_of_integers or [None])[len(list_of_integers) - 1]
