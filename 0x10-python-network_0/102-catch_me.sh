559bbaad9942df875d4c4e36ed56c8f71d579847
jeremy302 <olajeremy123@gmail.com>
2022-12-15 19:59:58 +0100

    patch

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 # answer file
 curl -s -X PUT -L --max-redirs -1 -H 'Origin: School' -d "user_id=98" "0.0.0.0:5000/catch_me"
-
-
-# curl -v -s --no-keepalive --post301 --post302 --post303 -X PUT -L -H 'Origin: School' -d "user_id=98" "0.0.0.0:5000/catch_me"
-
-# curl -v -s --no-keepalive --post301 --post302 --post303 -X PUT -L "0.0.0.0:5000/catch_me" -d "user_id=98"
-
-# curl -s -H 'Origin: School' -L -d "user_id=98" "0.0.0.0:5000/catch_me" -X PUT
