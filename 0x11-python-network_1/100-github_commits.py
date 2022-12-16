a71f8f888cc4e5cc4d635a74ef005cae0abe7ec7
jeremy302 <olajeremy123@gmail.com>
2022-12-16 13:11:06 +0100

    patch

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,7 +6,7 @@ import requests
 if __name__ == '__main__':
     repo, owner = sys.argv[1:3]
     res = requests.get(f'https://api.github.com/repos/{owner}/{repo}/commits',
-                       auth=(user, passwd), params={'per_page': 10})
+                       params={'per_page': 10})
     for item in res.json():
         commit = item['commit']
         sha = item['sha']
