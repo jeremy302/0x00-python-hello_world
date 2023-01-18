cc565f5dddbb09acddccb5187214fe6d42a9a2ed
jeremy302 <olajeremy123@gmail.com>
2023-01-18 05:48:52 +0100

    patch

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -5,15 +5,16 @@ const request = require('request');
 if (process.argv.length > 2) {
   const movie = process.argv[2];
 
-  request(`'https://swapi-api.hbtn.io/api'/films/${movie}/`,
+  request(`https://swapi-api.hbtn.io/api/films/${movie}/`,
     (e, r, b) => {
       e && console.log(e);
 
       const bd = JSON.parse(b);
       for (const v of bd.characters) {
-        request(v, (e, r, b2) => {
+        request(v, (e, r, b) => {
+          const bd2 = JSON.parse(b);
           e && console.log(e);
-          console.log(b2.name);
+          console.log(bd2.name);
         });
       }
     });
