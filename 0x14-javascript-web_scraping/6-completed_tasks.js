3aec16870561b633291941b1c761091f1b226a82
jeremy302 <olajeremy123@gmail.com>
2023-01-18 05:45:50 +0100

    patch

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -5,9 +5,17 @@ const request = require('request');
 if (process.argv.length > 2) {
   const url = process.argv[2];
   request(url, (e, r, b) => {
-    b = JSON.parse(b);
     e && console.log(e);
-    console.log(b.reduce((acc, v) =>
-      v.completed ? { ...acc, [v.userId]: (acc[v.userId] ?? 0) + 1 } : acc, {}));
+    const map = {};
+    const bd = JSON.parse(b);
+    for (const item of bd) {
+      if (item.completed) {
+        if (!map[item.userId]) {
+          map[item.userId] = 0;
+        }
+        map[item.userId]++;
+      }
+    }
+    console.log(map);
   });
 }
