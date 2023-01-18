4afd8ad19f24cad7626c844759c89f325757bb90
jeremy302 <olajeremy123@gmail.com>
2023-01-18 03:18:17 +0100

    patch

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,6 +4,6 @@ const fs = require('fs');
 
 if (process.argv.length > 2) {
   fs.readFile(process.argv[2], (e, v) => {
-    console.log(e || e.toString('utf-8'));
+    console.log(e || v.toString('utf-8'));
   });
 }
